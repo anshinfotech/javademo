35ff8e2f898b08922a692ad153aed2568981b4f6
grvgrewal <grv.grewal@gmail.com>
2020-01-05 19:34:54 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,9 @@
 package com.mycompany.app;
-
+import java.awt.FlowLayout;  
+import javax.swing.JButton;  
+import javax.swing.JFrame;  
+import javax.swing.JLabel;  
+import javax.swing.Jpanel;  
 /**
  * Hello world!
  */
@@ -11,6 +15,19 @@ public class App
     public App() {}
 
     public static void main(String[] args) {
+JFrame frame = new JFrame("JFrame Example");  
+        JPanel panel = new JPanel();  
+        panel.setLayout(new FlowLayout());  
+        JLabel label = new JLabel("JFrame By Example");  
+        JButton button = new JButton();  
+        button.setText("Button");  
+        panel.add(label);  
+        panel.add(button);  
+        frame.add(panel);  
+        frame.setSize(200, 300);  
+        frame.setLocationRelativeTo(null);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        frame.setVisible(true);
         System.out.println(new App().getMessage());
     }
 
