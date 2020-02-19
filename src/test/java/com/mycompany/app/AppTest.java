d6c20d2b6e66a96a5adc1396a5ea152da8384b01
anshinfotech <52543676+anshinfotech@users.noreply.github.com>
2020-02-19 16:41:45 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,10 +34,10 @@ public class AppTest
     {
         App.main(null);
  try {
-           assertEquals("Welcome to ANSH InfoTech" + System.getProperty("line.separator"),outContent.toString());
+           assertEquals("Welcome" + System.getProperty("line.separator"),outContent.toString());
 
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
+            fail("\"message\" is not \"Welcome\"");
         }
     }
 
