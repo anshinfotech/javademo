a16ed4b2fc7d902e6c9c2279b69ccd976dcf8e8a
anshinfotech <52543676+anshinfotech@users.noreply.github.com>
2020-02-20 12:30:15 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
  try {
-           assertEquals("Welcome" + System.getProperty("line.separator"),outContent.toString());
+           assertEquals("Welcome to AIT" + System.getProperty("line.separator"),outContent.toString());
 
         } catch (AssertionError e) {
             fail("\"message\" is not \"Welcome\"");
